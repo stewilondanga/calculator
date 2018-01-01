0e20338693066486e4b2ade8d55e9a33c037df0e
Stewart ilondanga <stilondanga@gmail.com>
2018-01-01 09:46:15 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
+$(document).ready(function() {
+  var displayTotal = 0;
+  //what is displayed on screen
+  var runningTotal = [];
+  //global var for previous calculations
+  var deciChecker = false;
+
+
+
 var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
