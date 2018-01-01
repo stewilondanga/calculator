9c1af29aa63962b41d9d450c0fb00d10f3191afa
Stewart ilondanga <stilondanga@gmail.com>
2018-01-01 09:48:56 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,39 @@ $(document).ready(function() {
 		    }
 		    switch (input) {
 
-
+					//switch for input from button click
+			       //cases for each number
+			       case '1':
+			         displayTotal += '1';
+			         break;
+			       case '2':
+			         displayTotal += '2';
+			         break;
+			       case '3':
+			         displayTotal += '3';
+			         break;
+			       case '4':
+			         displayTotal += '4';
+			         break;
+			       case '5':
+			         displayTotal += '5';
+			         break;
+			       case '6':
+			         displayTotal += '6';
+			         break;
+			       case '7':
+			         displayTotal += '7';
+			         break;
+			       case '8':
+			         displayTotal += '8';
+			         break;
+			       case '9':
+			         displayTotal += '9';
+			         break;
+			       case '0':
+			         displayTotal += '0';
+			         break;
+							 
 
 var navigate = (function() {
 	$('.dd').toggle();
